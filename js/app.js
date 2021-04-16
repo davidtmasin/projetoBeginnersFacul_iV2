872376b8b6631138dafb58e2c9fb2e16ac1eb68e
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-16 00:39:22 -0300

    Função txtQMuda em JS + Ajuste final de CSS

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var textoQueMuda = document.querySelector('#about p');
+
+textoQueMuda.addEventListener('click', function() {
+    textoQueMuda.innerHTML = '<a target="_blank" href="http://davidtmasin.github.io/" id="class">Clique aqui e veja mais!</a>'
+})
