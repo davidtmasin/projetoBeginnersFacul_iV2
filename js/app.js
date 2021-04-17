023290b686857e712b0c6bf7027734ffbdee1df7
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-17 00:03:17 -0300

    Projeto do Módulo 02 - Beginners Construído

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var textoQueMuda = document.querySelector('#about p');
+// var textoQueMuda = document.querySelector('#about p');
 
-textoQueMuda.addEventListener('click', function() {
-    textoQueMuda.innerHTML = '<a target="_blank" href="http://davidtmasin.github.io/" id="class">Clique aqui e veja mais!</a>'
-})
+// textoQueMuda.addEventListener('click', function() {
+//     textoQueMuda.innerHTML = '<a target="_blank" href="http://davidtmasin.github.io/" id="class">Clique aqui e veja mais!</a>'
+// })
+
+function calculoConversao() {
+	
+	var valorDolar = document.getElementById('dolar').value
+	var dolarValor = Number(valorDolar.replace(',','.'))
+
+	document.getElementById('resultado').value = (dolarValor * 5.51).toFixed(2).replace('.',',')
+
+}
+
+function limparValores() {
+	
+	document.getElementById('dolar').value = ""
+	
+	document.getElementById('resultado').value = ""
+	
+}
